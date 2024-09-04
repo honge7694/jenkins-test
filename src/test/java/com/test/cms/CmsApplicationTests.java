4e81903963e14d887511d0c67deb45874fef71e9
hui7694 <hui7694@creamhouse.net>
2024-09-04 13:13:47 +0900

    init commit

---
+++ src/test/java/com/test/cms/CmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
