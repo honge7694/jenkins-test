aaf9a4da0f4c73a0048083848772c0a1b8113705
hui7694 <hui7694@creamhouse.net>
2024-09-23 22:48:03 +0900

    Nginx 추가 및 Docker-compose 적용

--- deploy.sh
+++ deploy.sh
@@ -8,33 +8,21 @@ CONTAINER_NAME=cms-app-container
 PORT=8080
 JAR_FILE="cms-0.0.1-SNAPSHOT.jar"
 
-# 이전에 실행 중인 컨테이너 중지 및 삭제
-if [ "$(docker ps -q -f name=${CONTAINER_NAME})" ]; then
-  echo "실행중인 애플리케이션을 정지 후 삭제합니다."
-  docker stop ${CONTAINER_NAME}
-  docker rm ${CONTAINER_NAME}
-fi
-
-# 이전에 존재하는 이미지 삭제
-if [ "$(docker images -q ${IMAGE_NAME})" ]; then
-  echo "기존의 이미지를 삭제합니다."
-  docker rmi ${IMAGE_NAME}
-fi
+# 이전에 실행 중인 컨테이너 중지 및 삭제 (Compose 사용)
+echo "Docker Compose를 사용하여 기존 컨테이너 중지 및 삭제 중..."
+docker-compose down
 
 # JAR 파일을 현재 디렉토리로 복사
+echo "빌드된 JAR 파일을 복사 중..."
 cp /home/ubuntu/build/libs/${JAR_FILE} .
 
-# Docker 이미지를 빌드
-echo "Building new Docker Image..."
-docker build -t ${IMAGE_NAME} --build-arg JAR_FILE=${JAR_FILE} .
-
-# 새 컨테이너를 실행
-echo "Starting new container..."
-docker run -d --name ${CONTAINER_NAME} -p ${PORT}:8080 ${IMAGE_NAME}
+# Docker Compose를 사용하여 새 이미지 빌드 및 컨테이너 실행
+echo "Docker Compose로 새로운 이미지 빌드 및 컨테이너 실행 중..."
+docker-compose up --build -d
 
 # 배포 상태 확인
 if [ "$(docker ps -q -f name=${CONTAINER_NAME})" ]; then
-  echo "배포 성공! 어플리케이션 실행합니다. PORT : ${PORT}"
+  echo "배포 성공! 어플리케이션이 실행 중입니다. PORT : ${PORT}"
 else
   echo "배포 실패"
   exit 1
